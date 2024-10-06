f33245de09690452a0e93d576feee49620ea56a9
sanjeevmusyakhwa <sanjeevmusyakhwa@gmail.com>
2024-10-06 07:35:10 +0545

    Added forms.py ancd boookUpdate

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.bookList, name='bookList'),
     path('bookCreate/', views.bookCreate, name='bookCreate'),
     path('bookDelete/<int:pk>/', views.bookDelete, name='bookDelete'),
+    path('bookUpdate/<int:pk>/', views.BookUpdate, name='bookUpdate'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from app.models import Book
 from app.forms import BookForm
 # Create your views here.
@@ -29,3 +29,16 @@ def bookDelete(request,pk):
   book = Book.objects.get(id =pk)
   book.delete()
   return redirect('bookList')
+
+def BookUpdate(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)  
+        if form.is_valid():
+            form.save() 
+            return redirect('bookList') 
+    else:
+        form = BookForm(instance=book) 
+
+    return render(request, 'bookUpdate.html', {'form': form, 'book':book})
